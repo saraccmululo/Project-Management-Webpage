20e6d2b761feb06c14491aa2f65748b25cb855df
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-04-08 16:18:19 -0500

    Created ProjectSideBar, NoProjectSelected, Button, Input and NewProject components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSideBar/>
+      <NoProjectSelected />
+    </main>
   );
 }
 
